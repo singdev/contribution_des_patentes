f385dfe406ae39bc7dcf94adfcd8184d68343198
Nve Hounkponou Orphee <orpheenve@hotmail.com>
2020-11-09 08:41:07 +0100

    Ajouts des pages initiales

---
+++ page11.js
@@ -0,0 +1,218 @@
+module.exports = [
+    {
+        str: "Commissionnaire en marchandise",
+        lbv_pog:85000,
+        autre_commune:85000,
+        departement:60000,
+    },
+    {
+        str: "Compagnie de navigation(aérienne, maritime ou fluviale)(voir navigation)",
+        lbv_pog:
+        autre_commune:
+        departement:
+    },
+    {
+        str: "Compagnie de navigation(aérienne, maritime ou fluviale) - "
+        lbv_pog:
+        autre_commune:
+        departement:
+    },
+    {
+        str: "Compagnie de navigation(aérienne, maritime ou fluviale) -"
+        lbv_pog:
+        autre_commune:
+        departement:
+    },
+    {
+        str: "Comptabilité(Société de)",
+        lbv_pog:0,
+        autre_commune:0,
+        departement:0,
+    },
+    {
+        str: "Comptabilité(Société de) - établissement principal du territoire",
+        lbv_pog:200000,
+        autre_commune:200000,
+        departement:200000,
+    },
+    {
+        str: "Comptabilité(Société de) - établissement secondaire du territoire",
+        lbv_pog:170000,
+        autre_commune:170000,
+        departement:170000,
+    },
+    {
+        str: "Comptable agréé",
+        lbv_pog:0,
+        autre_commune:0,
+        departement:0,
+    },
+    {
+        str: "Comptable agréé - employant plus d'une personne",
+        lbv_pog:110000,
+        autre_commune:110000,
+        departement:110000,
+    },
+    {
+        str: "Comptable agréé - employant une personne",
+        lbv_pog:85000,
+        autre_commune:85000,
+        departement:60000,
+    },
+    {
+        str: "Comptable agréé - travaillant seul",
+        lbv_pog:65000,
+        autre_commune:65000,
+        departement:55000,
+    },
+    {
+        str: "Concessionnaire d'entrepôt",
+        lbv_pog:110000,
+        autre_commune:110000,
+        departement:110000,
+    },
+    {
+        str: "Conseil",
+        lbv_pog:0,
+        autre_commune:0,
+        departement:0,
+    },
+    {
+        str: "Conseil - employant plus d'une personne",
+        lbv_pog:170000,
+        autre_commune:170000,
+        departement:170000,
+    },
+    {
+        str: "Conseil - employant une personne",
+        lbv_pog:110000,
+        autre_commune:110000,
+        departement:110000,
+    },
+    {
+        str: "Conseil - travaillant seul",
+        lbv_pog:85000,
+        autre_commune:85000,
+        departement:60000,
+    },
+    {
+        str: "Consignataire de navires ou d'avions",
+        lbv_pog:85000,
+        autre_commune:85000,
+        departement:60000,
+    },
+    {
+        str: "Cordonnier, maroquinier",
+        lbv_pog:0,
+        autre_commune:0,
+        departement:0,
+    },
+    {
+        str: "Cordonnier, maroquinier - important tout ou partie des produits nécessaires à la fabrication",
+        lbv_pog:65000,
+        autre_commune:65000,
+        departement:55000,
+    },
+    {
+        str: "Cordonnier, maroquinier - n'important pas(voir Artisan)",
+        lbv_pog:65000,
+        autre_commune:65000,
+        departement:55000,
+    },
+    {
+        str: "Cordonnier, maroquinier - ambulant",
+        lbv_pog:15000,
+        autre_commune:15000,
+        departement:12000,
+    },
+    {
+        str: "Courtier",
+        lbv_pog:85000,
+        autre_commune:85000,
+        departement:60000,
+    },
+    {
+        str: "Couturière ayant un établissement de vente",
+        lbv_pog:40000,
+        autre_commune:40000,
+        departement:30000,
+    },
+    {
+        str: "Couturière ayant un établissement de vente - par machine",
+        lbv_pog:40000,
+        autre_commune:40000,
+        departement:30000,
+    },
+    {
+        str: "Couturière ayant un établissement de vente - par machine en sus de 3",
+        lbv_pog:40000,
+        autre_commune:40000,
+        departement:30000,
+    },
+    {
+        str: "Couturière ayant un établissement de vente - par personne employée",
+        lbv_pog:40000,
+        autre_commune:40000,
+        departement:30000,
+    },
+    {
+        str: "Couturière en chambre",
+        lbv_pog:15000,
+        autre_commune:15000,
+        departement:12000,
+    },
+    {
+        str: "Couturière en chambre - par machine",
+        lbv_pog:15000,
+        autre_commune:15000,
+        departement:12000,
+    },
+    {
+        str: "Couturière en chambre - par machine en sus de 3",
+        lbv_pog:15000,
+        autre_commune:15000,
+        departement:12000,
+    },
+    {
+        str: "Couturier, tailleur ambulant",
+        lbv_pog:20000,
+        autre_commune:15000,
+        departement:10000,
+    },
+    {
+        str: "Couvreur",
+        lbv_pog:0,
+        autre_commune:0,
+        departement:0,
+    },
+    {
+        str: "Couvreur - travaillant seul",
+        lbv_pog:40000,
+        autre_commune:40000,
+        departement:35000,
+    },
+    {
+        str: "Couvreur - employant 1 à 5 personnes",
+        lbv_pog:65000,
+        autre_commune:65000,
+        departement:55000,
+    },
+    {
+        str: "Crédit mobilier ou immobilier",
+        lbv_pog:0,
+        autre_commune:0,
+        departement:0,
+    },
+    {
+        str: "Crédit mobilier ou immobilier - établissement principal du territoire",
+        lbv_pog:200000,
+        autre_commune:200000,
+        departement:200000,
+    },
+    {
+        str: "Crédit mobilier ou immobilier - établissement secondaire du territoire",
+        lbv_pog:110000,
+        autre_commune:110000,
+        departement:110000,
+    },
+]
